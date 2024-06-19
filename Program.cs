996f0011e232dbdbb36fde3d3c273be155891bd2
Dmitriy209 <dim286754@gmail.com>
2024-06-19 20:51:31 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammUnderPassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int triesCount = 3;
+
+            string password = "password12345qwerty";
+            string protectInformation = "Город под подшвой, а программа под паролем";
+
+            for (int i = 0; i < triesCount; i++)
+            {
+                Console.WriteLine("Введите пароль:");
+                string userInput = Console.ReadLine();
+
+                if (userInput == password)
+                {
+                    Console.WriteLine($"Пароль принят.\n" +
+                        $"{protectInformation}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели неправильный пароль,\n" +
+                        "У вас осталось " + (triesCount - (i + 1)) + " попыток");
+                }
+            }
+        }
+    }
+}
+
+
+
